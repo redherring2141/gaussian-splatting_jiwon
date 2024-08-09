c4a7ba33a2c228235b18e233228c8293e262141d
redherring2141 <redherring2141@kaist.ac.kr>
2024-08-09 14:34:13 +0900

    Progress save after CAL paper revision 1st draft and before arrange things

---
+++ change_models_cfgargs.sh
@@ -0,0 +1,5 @@
+for DATA in drjohnson playroom train truck bicycle bonsai counter flowers garden kitchen room stump treehill mic chair ship materials lego drums ficus hotdog
+do
+	cd /home/jetson-agx/NeRF/models/$DATA\_30k/
+	sed -i 's/work6\/jiwon/home\/jetson-agx/g' cfg_args
+done
