0e9fc29b2791b09837d045f74d12c236de4784d2
redherring2141 <redherring2141@kaist.ac.kr>
2024-08-13 17:20:39 +0900

    Added script options for A100 measurement.

--- scripts/CAL_scripts/Fig2/Fig2_RenderE2ELatencyBrkdown.sh
+++ scripts/CAL_scripts/Fig2/Fig2_RenderE2ELatencyBrkdown.sh
@@ -10,11 +10,19 @@ then
 	exit 1
 else
 	echo "Processing $1 ..."
-	python3 /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/render_profiling.py -m /home/jetson-agx/NeRF/models/$1/ --skip_train --CUDAEVENT > ./fig2_data_20240812/log_render_CUDAEVENT_$1_$2
-	python3 ./Fig2_LogParser.py ./fig2_data_20240812/log_render_CUDAEVENT_$1_$2 ./fig2_data_20240812/summary_render_CUDAEVENT.txt
-	python3 /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/render.py -m /home/jetson-agx/NeRF/models/$1/ --skip_train
-	python3 /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/metrics_profiling.py -m /home/jetson-agx/NeRF/models/$1/ > ./fig2_data_20240812/log_metric_CUDAEVENT_$1_$2
-	python3 ./Fig2_LogParser.py ./fig2_data_20240812/log_metric_CUDAEVENT_$1_$2 ./fig2_data_20240812/summary_render_CUDAEVENT.txt
+	#A100
+	python3 /work6/jiwon/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/render_profiling.py -m /work6/jiwon/NeRF/output_full/$1/ --skip_train --CUDAEVENT > ./fig2_A100_20240813/log_render_CUDAEVENT_$1_$2
+	python3 ./Fig2_LogParser.py ./fig2_A100_20240813/log_render_CUDAEVENT_$1_$2 ./fig2_A100_20240813/summary_render_CUDAEVENT.txt
+	python3 /work6/jiwon/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/render.py -m /work6/jiwon/NeRF/output_full/$1/ --skip_train
+	python3 /work6/jiwon/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/metrics_profiling.py -m /work6/jiwon/NeRF/output_full/$1/ > ./fig2_A100_20240813/log_metric_CUDAEVENT_$1_$2
+	python3 ./Fig2_LogParser.py ./fig2_A100_20240813/log_metric_CUDAEVENT_$1_$2 ./fig2_A100_20240813/summary_render_CUDAEVENT.txt
+
+	#Xavier-AGX
+	# python3 /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/render_profiling.py -m /home/jetson-agx/NeRF/models/$1/ --skip_train --CUDAEVENT > ./fig2_XavierAGX_20240812/log_render_CUDAEVENT_$1_$2
+	# python3 ./Fig2_LogParser.py ./fig2_XavierAGX_20240812/log_render_CUDAEVENT_$1_$2 ./fig2_XavierAGX_20240812/summary_render_CUDAEVENT.txt
+	# python3 /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/render.py -m /home/jetson-agx/NeRF/models/$1/ --skip_train
+	# python3 /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/metrics_profiling.py -m /home/jetson-agx/NeRF/models/$1/ > ./fig2_XavierAGX_20240812/log_metric_CUDAEVENT_$1_$2
+	# python3 ./Fig2_LogParser.py ./fig2_XavierAGX_20240812/log_metric_CUDAEVENT_$1_$2 ./fig2_XavierAGX_20240812/summary_render_CUDAEVENT.txt
 fi
 }
 
