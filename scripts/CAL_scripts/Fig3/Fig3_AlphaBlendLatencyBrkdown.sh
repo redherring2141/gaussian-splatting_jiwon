b2aa47f25fb243158597a80b78859ce4ad40c932
redherring2141 <redherring2141@kaist.ac.kr>
2024-08-13 21:54:21 +0900

    Added options for A100 and XavierAGX in Fig3 script.

--- scripts/CAL_scripts/Fig3/Fig3_AlphaBlendLatencyBrkdown.sh
+++ scripts/CAL_scripts/Fig3/Fig3_AlphaBlendLatencyBrkdown.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
+
+str1 = "A100"
+str2 = "Xavier"
+
 function BlendingLatencyBrkdown(){
-if [ $# -lt 3 ];
+if [ $# -lt 4 ];
 then
 	echo "$0: Missing arguments"
 	exit 1
-elif [ $# -gt 3 ];
+elif [ $# -gt 4 ];
 then
 	echo "$0: Too many arguments"
 	exit 1
 else
-	echo "Processing $1_$2_$3 ..."
-	#A100
-	python3 /work6/jiwon/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/render_profiling.py -m /work6/jiwon/NeRF/output_full/$1/ --skip_train --CUDAEVENT > ../../../../fig3_A100_20240813/log_render_CUDAEVENT_$1_$2_$3
-	python3 ./Fig3_LogParse.py ../../../../fig3_A100_20240813/log_render_CUDAEVENT_$1_$2_$3 ../../../../fig3_A100_20240813/summary_blending.txt
-	python3 /work6/jiwon/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/render.py -m /work6/jiwon/NeRF/output_full/$1/ --skip_train
-	python3 /work6/jiwon/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/metrics_profiling.py -m /work6/jiwon/NeRF/output_full/$1/ > ../../../../fig3_A100_20240813/log_metric_CUDAEVENT_$1_$2_$3
-	python3 ./Fig3_LogParse.py ../../../../fig3_A100_20240813/log_metric_CUDAEVENT_$1_$2_$3 ../../../../fig3_A100_20240813/summary_blending.txt
+	echo "Processing $1_$2_$3 on $4..."
 
-	#Xavier-AGX
-	# python3 /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/render_profiling.py -m /home/jetson-agx/NeRF/models/$1/ --skip_train --CUDAEVENT > ../../../../fig3_XavierAGX_20240813/log_render_CUDAEVENT_$1_$2_$3
-	# python3 ./Fig3_LogParse.py ../../../../fig3_XavierAGX_20240813/log_render_CUDAEVENT_$1_$2_$3 ../../../../fig3_XavierAGX_20240813/summary_blending.txt
-	# python3 /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/render.py -m /home/jetson-agx/NeRF/models/$1/ --skip_train
-	# python3 /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/metrics_profiling.py -m /home/jetson-agx/NeRF/models/$1/ > ../../../../fig3_XavierAGX_20240813/log_metric_CUDAEVENT_$1_$2_$3
-	# python3 ./Fig3_LogParse.py ../../../../fig3_XavierAGX_20240813/log_metric_CUDAEVENT_$1_$2_$3 ../../../../fig3_XavierAGX_20240813/summary_blending.txt
+	if [ $4 == $str1 ];
+	then
+		#A100
+		python3 /work6/jiwon/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/render_profiling.py -m /work6/jiwon/NeRF/output_full/$1/ --skip_train --CUDAEVENT > ../../../../fig3_A100_20240813/log_render_CUDAEVENT_$1_$2_$3
+		python3 ./Fig3_LogParse.py ../../../../fig3_A100_20240813/log_render_CUDAEVENT_$1_$2_$3 ../../../../fig3_A100_20240813/summary_blending.txt
+		python3 /work6/jiwon/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/render.py -m /work6/jiwon/NeRF/output_full/$1/ --skip_train
+		python3 /work6/jiwon/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/metrics_profiling.py -m /work6/jiwon/NeRF/output_full/$1/ > ../../../../fig3_A100_20240813/log_metric_CUDAEVENT_$1_$2_$3
+		python3 ./Fig3_LogParse.py ../../../../fig3_A100_20240813/log_metric_CUDAEVENT_$1_$2_$3 ../../../../fig3_A100_20240813/summary_blending.txt
+	else
+		#Xavier-AGX
+		python3 /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/render_profiling.py -m /home/jetson-agx/NeRF/models/$1/ --skip_train --CUDAEVENT > ../../../../fig3_XavierAGX_20240813/log_render_CUDAEVENT_$1_$2_$3
+		python3 ./Fig3_LogParse.py ../../../../fig3_XavierAGX_20240813/log_render_CUDAEVENT_$1_$2_$3 ../../../../fig3_XavierAGX_20240813/summary_blending.txt
+		python3 /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/render.py -m /home/jetson-agx/NeRF/models/$1/ --skip_train
+		python3 /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/metrics_profiling.py -m /home/jetson-agx/NeRF/models/$1/ > ../../../../fig3_XavierAGX_20240813/log_metric_CUDAEVENT_$1_$2_$3
+		python3 ./Fig3_LogParse.py ../../../../fig3_XavierAGX_20240813/log_metric_CUDAEVENT_$1_$2_$3 ../../../../fig3_XavierAGX_20240813/summary_blending.txt
+	fi
 fi
 }
 
@@ -55,16 +63,19 @@ do
 
 		for DATA in drjohnson playroom train truck bicycle bonsai counter flowers garden kitchen room stump treehill
         do
-            BlendingLatencyBrkdown $DATA\_30k $FUNC $PART
+            BlendingLatencyBrkdown $DATA\_30k $FUNC $PART $1
             #echo $DATA\_30k $FUNC $PART
         done
     done
 done
 
-#A100
-cp /work6/jiwon/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/submodules/diff-gaussian-rasterization/cuda_rasterizer/forward_org.cu /work6/jiwon/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/submodules/diff-gaussian-rasterization/cuda_rasterizer/forward.cu
-pip install /work6/jiwon/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/submodules/diff-gaussian-rasterization
-
-#XavierAGX
-# cp /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/submodules/diff-gaussian-rasterization/cuda_rasterizer/forward_org.cu /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/submodules/diff-gaussian-rasterization/cuda_rasterizer/forward.cu
-# pip install /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/submodules/diff-gaussian-rasterization
+if [ $1 == $str1 ];
+then
+	#A100
+	cp /work6/jiwon/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/submodules/diff-gaussian-rasterization/cuda_rasterizer/forward_org.cu /work6/jiwon/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/submodules/diff-gaussian-rasterization/cuda_rasterizer/forward.cu
+	pip install /work6/jiwon/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/submodules/diff-gaussian-rasterization
+else
+	#XavierAGX
+	cp /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/submodules/diff-gaussian-rasterization/cuda_rasterizer/forward_org.cu /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/submodules/diff-gaussian-rasterization/cuda_rasterizer/forward.cu
+	pip install /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/submodules/diff-gaussian-rasterization
+fi
