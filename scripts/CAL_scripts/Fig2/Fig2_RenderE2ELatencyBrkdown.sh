1f8c4a1a3050573fdc40a346aac06bd14ca67102
redherring2141 <redherring2141@kaist.ac.kr>
2024-08-13 15:03:21 +0900

    Re-prepared the scripts and settings for re-evaluation data measurement of paper revision.

--- scripts/CAL_scripts/Fig2/Fig2_RenderE2ELatencyBrkdown.sh
+++ scripts/CAL_scripts/Fig2/Fig2_RenderE2ELatencyBrkdown.sh
@@ -10,16 +10,16 @@ then
 	exit 1
 else
 	echo "Processing $1 ..."
-	python3 ./render_profiling.py -m /home/jetson-agx/NeRF/models/$1/ --skip_train --CUDAEVENT > /home/jetson-agx/NeRF/gaussian-splatting_jiwon/measurements/log_20240528_fig1/log_render_CUDAEVENT_$1_$2
-	python3 ./Fig1_LogParser.py /home/jetson-agx/NeRF/gaussian-splatting_jiwon/measurements/log_20240528_fig1/log_render_CUDAEVENT_$1_$2
-	python3 ./render.py -m /home/jetson-agx/NeRF/models/$1/ --skip_train
-	python3 ./metrics_profiling.py -m /home/jetson-agx/NeRF/models/$1/ > /home/jetson-agx/NeRF/gaussian-splatting_jiwon/measurements/log_20240528_fig1/log_metric_CUDAEVENT_$1_$2
-	python3 ./Fig1_LogParser.py /home/jetson-agx/NeRF/gaussian-splatting_jiwon/measurements/log_20240528_fig1/log_metric_CUDAEVENT_$1_$2
+	python3 /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/render_FPS.py -m /home/jetson-agx/NeRF/models/$1/ --skip_train > ./fig2_data_20240812/log_render_CUDAEVENT_$1_$2
+	python3 ./Fig2_LogParser.py ./fig2_data_20240812/log_render_CUDAEVENT_$1_$2 ./fig2_data_20240812/summary_render_CUDAEVENT.txt
+	python3 /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/render.py -m /home/jetson-agx/NeRF/models/$1/ --skip_train
+	python3 /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/metrics_profiling.py -m /home/jetson-agx/NeRF/models/$1/ > ./fig2_data_20240812/log_metric_CUDAEVENT_$1_$2
+	python3 ./Fig2_LogParser.py ./fig2_data_20240812/log_metric_CUDAEVENT_$1_$2 ./fig2_data_20240812/summary_render_CUDAEVENT.txt
 fi
 }
 
-for DATA in drjohnson playroom train truck bicycle bonsai counter flowers garden kitchen room stump treehill mic chair ship materials lego drums ficus hotdog
-#for DATA in room stump treehill mic chair ship materials lego drums ficus hotdog
+#for DATA in drjohnson playroom train truck bicycle bonsai counter flowers garden kitchen room stump treehill mic chair ship materials lego drums ficus hotdog
+for DATA in room #stump treehill mic chair ship materials lego drums ficus hotdog
 do
 	E2eLatencyBrkdown $DATA\_30k exp #$FUNC $PART
 	#echo $DATA\_30k $FUNC $PART
