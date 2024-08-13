331b45c01772b331b2344223c62f7ff2403e7558
redherring2141 <redherring2141@kaist.ac.kr>
2024-08-13 16:26:16 +0900

    Reverted Fig.2 script to measure end-to-end latency breakdown.

--- scripts/CAL_scripts/Fig2/Fig2_RenderE2ELatencyBrkdown.sh
+++ scripts/CAL_scripts/Fig2/Fig2_RenderE2ELatencyBrkdown.sh
@@ -10,7 +10,7 @@ then
 	exit 1
 else
 	echo "Processing $1 ..."
-	python3 /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/render_FPS.py -m /home/jetson-agx/NeRF/models/$1/ --skip_train > ./fig2_data_20240812/log_render_CUDAEVENT_$1_$2
+	python3 /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/render_profiling.py -m /home/jetson-agx/NeRF/models/$1/ --skip_train --CUDAEVENT > ./fig2_data_20240812/log_render_CUDAEVENT_$1_$2
 	python3 ./Fig2_LogParser.py ./fig2_data_20240812/log_render_CUDAEVENT_$1_$2 ./fig2_data_20240812/summary_render_CUDAEVENT.txt
 	python3 /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/render.py -m /home/jetson-agx/NeRF/models/$1/ --skip_train
 	python3 /home/jetson-agx/NeRF/CAL_3DGS_rev/gaussian-splatting_jiwon/metrics_profiling.py -m /home/jetson-agx/NeRF/models/$1/ > ./fig2_data_20240812/log_metric_CUDAEVENT_$1_$2
